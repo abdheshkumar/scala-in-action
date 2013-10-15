3b793170b1238be24c53ec60a4754e8fb928fb40
Trond Bjerkestrand <trond.bjerkestrand@typesafe.com>
2013-10-15 09:39:28 +0200

    Adding Slides.

---
+++ src/main/scala/ScalaInAction.scala
@@ -0,0 +1,51 @@
+object ScalaInAction {
+  println("""
+
+   SSSSSSSSSSSSSSS                                       lllllll                          iiii                   
+ SS:::::::::::::::S                                      l:::::l                         i::::i                  
+S:::::SSSSSS::::::S                                      l:::::l                          iiii                   
+S:::::S     SSSSSSS                                      l:::::l                                                 
+S:::::S                cccccccccccccccc  aaaaaaaaaaaaa    l::::l   aaaaaaaaaaaaa        iiiiiiinnnn  nnnnnnnn    
+S:::::S              cc:::::::::::::::c  a::::::::::::a   l::::l   a::::::::::::a       i:::::in:::nn::::::::nn  
+ S::::SSSS          c:::::::::::::::::c  aaaaaaaaa:::::a  l::::l   aaaaaaaaa:::::a       i::::in::::::::::::::nn 
+  SS::::::SSSSS    c:::::::cccccc:::::c           a::::a  l::::l            a::::a       i::::inn:::::::::::::::n
+    SSS::::::::SS  c::::::c     ccccccc    aaaaaaa:::::a  l::::l     aaaaaaa:::::a       i::::i  n:::::nnnn:::::n
+       SSSSSS::::S c:::::c               aa::::::::::::a  l::::l   aa::::::::::::a       i::::i  n::::n    n::::n
+            S:::::Sc:::::c              a::::aaaa::::::a  l::::l  a::::aaaa::::::a       i::::i  n::::n    n::::n
+            S:::::Sc::::::c     ccccccca::::a    a:::::a  l::::l a::::a    a:::::a       i::::i  n::::n    n::::n
+SSSSSSS     S:::::Sc:::::::cccccc:::::ca::::a    a:::::a l::::::la::::a    a:::::a      i::::::i n::::n    n::::n
+S::::::SSSSSS:::::S c:::::::::::::::::ca:::::aaaa::::::a l::::::la:::::aaaa::::::a      i::::::i n::::n    n::::n
+S:::::::::::::::SS   cc:::::::::::::::c a::::::::::aa:::al::::::l a::::::::::aa:::a     i::::::i n::::n    n::::n
+ SSSSSSSSSSSSSSS       cccccccccccccccc  aaaaaaaaaa  aaaallllllll  aaaaaaaaaa  aaaa     iiiiiiii nnnnnn    nnnnnn
+
+    
+               AAA                                         tttt            iiii                                        !!! 
+              A:::A                                     ttt:::t           i::::i                                      !!:!!
+             A:::::A                                    t:::::t            iiii                                       !:::!
+            A:::::::A                                   t:::::t                                                       !:::!
+           A:::::::::A            ccccccccccccccccttttttt:::::ttttttt    iiiiiii    ooooooooooo   nnnn  nnnnnnnn      !:::!
+          A:::::A:::::A         cc:::::::::::::::ct:::::::::::::::::t    i:::::i  oo:::::::::::oo n:::nn::::::::nn    !:::!
+         A:::::A A:::::A       c:::::::::::::::::ct:::::::::::::::::t     i::::i o:::::::::::::::on::::::::::::::nn   !:::!
+        A:::::A   A:::::A     c:::::::cccccc:::::ctttttt:::::::tttttt     i::::i o:::::ooooo:::::onn:::::::::::::::n  !:::!
+       A:::::A     A:::::A    c::::::c     ccccccc      t:::::t           i::::i o::::o     o::::o  n:::::nnnn:::::n  !:::!
+      A:::::AAAAAAAAA:::::A   c:::::c                   t:::::t           i::::i o::::o     o::::o  n::::n    n::::n  !:::!
+     A:::::::::::::::::::::A  c:::::c                   t:::::t           i::::i o::::o     o::::o  n::::n    n::::n  !!:!!
+    A:::::AAAAAAAAAAAAA:::::A c::::::c     ccccccc      t:::::t    tttttt i::::i o::::o     o::::o  n::::n    n::::n   !!! 
+   A:::::A             A:::::Ac:::::::cccccc:::::c      t::::::tttt:::::ti::::::io:::::ooooo:::::o  n::::n    n::::n     
+  A:::::A               A:::::Ac:::::::::::::::::c      tt::::::::::::::ti::::::io:::::::::::::::o  n::::n    n::::n   !!! 
+ A:::::A                 A:::::Acc:::::::::::::::c        tt:::::::::::tti::::::i oo:::::::::::oo   n::::n    n::::n  !!:!!
+AAAAAAA                   AAAAAAA cccccccccccccccc          ttttttttttt  iiiiiiii   ooooooooooo     nnnnnn    nnnnnn   !!! 
+
+  
+	No slides - just Scala code!
+
+		JDD, Kraków
+
+		by Trond Bjerkestrand (@tbjerkes)
+		Typesafe
+  
+  
+""")
+  val start = new JavaTime(11, 0)
+  val end = Thanks
+}
---
+++ src/main/scala/Thanks.scala
@@ -0,0 +1,39 @@
+object Thanks {
+  println("""
+TTTTTTTTTTTTTTTTTTTTTTThhhhhhh                                                kkkkkkkk                
+T:::::::::::::::::::::Th:::::h                                                k::::::k                
+T:::::::::::::::::::::Th:::::h                                                k::::::k                
+T:::::TT:::::::TT:::::Th:::::h                                                k::::::k                
+TTTTTT  T:::::T  TTTTTT h::::h hhhhh         aaaaaaaaaaaaa  nnnn  nnnnnnnn     k:::::k    kkkkkkk     
+        T:::::T         h::::hh:::::hhh      a::::::::::::a n:::nn::::::::nn   k:::::k   k:::::k      
+        T:::::T         h::::::::::::::hh    aaaaaaaaa:::::an::::::::::::::nn  k:::::k  k:::::k       
+        T:::::T         h:::::::hhh::::::h            a::::ann:::::::::::::::n k:::::k k:::::k        
+        T:::::T         h::::::h   h::::::h    aaaaaaa:::::a  n:::::nnnn:::::n k::::::k:::::k         
+        T:::::T         h:::::h     h:::::h  aa::::::::::::a  n::::n    n::::n k:::::::::::k          
+        T:::::T         h:::::h     h:::::h a::::aaaa::::::a  n::::n    n::::n k:::::::::::k          
+        T:::::T         h:::::h     h:::::ha::::a    a:::::a  n::::n    n::::n k::::::k:::::k         
+      TT:::::::TT       h:::::h     h:::::ha::::a    a:::::a  n::::n    n::::nk::::::k k:::::k        
+      T:::::::::T       h:::::h     h:::::ha:::::aaaa::::::a  n::::n    n::::nk::::::k  k:::::k       
+      T:::::::::T       h:::::h     h:::::h a::::::::::aa:::a n::::n    n::::nk::::::k   k:::::k     !!!    
+      TTTTTTTTTTT       hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa nnnnnn    nnnnnnkkkkkkkk    kkkkkkk   !!:!!   
+                                                                                                    !:::!                                  
+                                                                                                    !:::!                                  
+                                    yyyyyyy           yyyyyyy ooooooooooo   uuuuuu    uuuuuu        !:::!                                  
+                                     y:::::y         y:::::yoo:::::::::::oo u::::u    u::::u        !:::!                                  
+                                      y:::::y       y:::::yo:::::::::::::::ou::::u    u::::u        !:::!                                  
+                                       y:::::y     y:::::y o:::::ooooo:::::ou::::u    u::::u        !:::!                                  
+                                        y:::::y   y:::::y  o::::o     o::::ou::::u    u::::u        !:::!                                  
+                                         y:::::y y:::::y   o::::o     o::::ou::::u    u::::u        !:::!                                  
+                                          y:::::y:::::y    o::::o     o::::ou::::u    u::::u        !!:!!                                  
+                                           y:::::::::y     o::::o     o::::ou:::::uuuu:::::u         !!!                                   
+                                            y:::::::y      o:::::ooooo:::::ou:::::::::::::::uu                                            
+                                            y:::::y         oo:::::::::::oo   uu::::::::uu:::u      !!:!!                                  
+                                           y:::::y            ooooooooooo       uuuuuuuu  uuuu       !!!                                   
+                                          y:::::y                                                                                         
+                                         y:::::y                                                                                          
+                                        y:::::y            Follow me: @tbjerkes                                                                                
+                                       y:::::y             Code: github.com/tbje/scalainaction                                                                                
+                                      yyyyyyy                                                                                             
+      """)
+
+}
